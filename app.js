fee83a968fb4287055a586e8c7615ef8b9091bd8
raiyannawaz <raiyannawaz95@gmail.com>
2024-12-09 12:37:08 +0530

    Final Commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 2000;
 const cors = require('cors')
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: 'https://raiyannawaz.github.io/MyNotebook'}))
 app.use('/api/user', require('./routes/user'))
 app.use('/api/note', require('./routes/note'))
 
